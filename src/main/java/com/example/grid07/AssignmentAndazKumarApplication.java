e8c3b348bbaecbb33a3c974c18a546bc5049b8bc
Rishav Pramanik <rishavpramanik17@gmail.com>
2026-05-08 23:54:20 +0530

    initial spring boot setup with postgres and redis dependencies

---
+++ src/main/java/com/example/grid07/AssignmentAndazKumarApplication.java
@@ -0,0 +1,13 @@
+package com.example.grid07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentAndazKumarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentAndazKumarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/grid07/AssignmentAndazKumarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grid07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentAndazKumarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
